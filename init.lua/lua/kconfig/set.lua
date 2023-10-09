73a6d37e132d1eefc8bcf206637c9552c1edcd97
konstantin <71085231+konstantinsGH@users.noreply.github.com>
2023-10-09 17:23:06 +0200

    Update set.lua

--- init.lua/lua/kconfig/set.lua
+++ init.lua/lua/kconfig/set.lua
@@ -15,7 +15,7 @@ vim.opt.wrap = false
 vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undofile = true
-vim.opt.undodir = "~/.config/nvim/undo/"
+vim.opt.undodir = vim.fn.expand('~/.config/nvim/undo/')
 
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
@@ -27,7 +27,7 @@ vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
 
 vim.opt.updatetime = 50
-vim.opt.colorcolumn = "80"
+-- vim.opt.colorcolumn = "80"
 
 vim.g.mapleader = " "
 
